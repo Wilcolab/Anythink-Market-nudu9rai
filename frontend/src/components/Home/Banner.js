10e832e713d164895dcadfd142fe8fd63a466f0b
Nico Landers <nico.landerss@gmail.com>
2023-07-24 12:11:56 +0200

    Add search bar

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,14 +1,16 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import SearchBar from "../SearchBar"
 
-const Banner = () => {
+const Banner = (onSearchBarChange) => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span>A place to </span>
-          <span id="get-part">get</span>
+          <span id="get-part">get </span>
+          <SearchBar id="search-box" onChange={onSearchBarChange} />
           <span> the cool stuff.</span>
         </div>
       </div>
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import agent from "../agent"
+
+const SearchBar = (onChange) => {
+    const [input, setInput] = useState("")
+
+    const handleChange = (value) => {
+        setInput(value)
+        if (value.length > 2) {
+            onChange(value, (page) => agent.Items.byTitle(value, page), agent.Items.byTitle(value))
+        }
+    }
+
+    return (
+        <input
+            key="search-bar"
+            value={input}
+            placeholder={"What is it that you truly desire?"}
+            onChange={(e) => handleChange(e.target.value)}
+        />
+    );
+}
+
+export default SearchBar;
