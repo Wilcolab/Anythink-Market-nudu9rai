27729022e99768ea87206adee9b190419f399028
Nico Landers <nico.landerss@gmail.com>
2023-07-25 14:02:37 +0200

    Update message when no items presented

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -8,7 +8,10 @@ const ItemList = (props) => {
   }
 
   if (props.items.length === 0) {
-    return <div className="py-4 no-items">No items are here... yet.</div>;
+    if (!props.value || props.value.length < 3) {
+      return <div className="py-4 no-items">No items are here... yet.</div>;
+    }
+    return <div className="py-4 no-items" id="empty" >No items found for "{props.value}"</div>;
   }
 
   return (
